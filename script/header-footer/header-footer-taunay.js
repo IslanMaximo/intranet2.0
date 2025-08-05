2d3abaa2b9603fd0d958866533d1fed7b3f43941
IslanMaximo <islanmvieira@gmail.com>
2025-08-05 11:35:38 -0300

    lista tel

--- script/header-footer/header-footer-taunay.js
+++ script/header-footer/header-footer-taunay.js
@@ -1,77 +1,79 @@
 window.onload = carregamento()
 let banner = document.querySelector('div#banner')
 function carregamento(){
-    let header = document.querySelector('header.principal')
-    let footer = document.querySelector('footer.principal')
+    let header = document.querySelector('header.taunay')
+    let footer = document.querySelector('footer.taunay')
 
     //header----------------------------------------
-    header.innerHTML = `<div id="navegacao">
-                <nav>
-                    <ul class="menu">
-                        <li>
-                            <a href="unidades/taunay/taunay.html">Taunay</a>
-                            <ul class="submenu">
-                                <li>
-                                    3091-8686            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Patio</a>
-                            <ul class="submenu">
-                                <li>
-                                    3030-7676            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Sete</a>
-                            <ul class="submenu">
-                                <li>
-                                    3242-3963            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Cabral</a>
-                            <ul class="submenu">
-                                <li>
-                                    3254-4224            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Juveve</a>
-                            <ul class="submenu">
-                                <li>
-                                    3254-6621            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Patio Men</a>
-                            <ul class="submenu">
-                                <li>
-                                    99774-0026            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Barigui</a>
-                            <ul class="submenu">
-                                <li>
-                                    99771-0437            
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">Barigui Men</a>
-                            <ul class="submenu">
-                                <li>
-                                    3441-0211            
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>`
+    header.innerHTML = `
+        <div id="navegacao">
+            <nav>
+                <ul class="menu">
+                    <li>
+                        <a href="taunay.html">Taunay</a>
+                        <ul class="submenu">
+                            <li>
+                                3091-8686            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../patio/patio.html">Patio</a>
+                        <ul class="submenu">
+                            <li>
+                                3030-7676            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../sete/sete.html">Sete</a>
+                        <ul class="submenu">
+                            <li>
+                                3242-3963            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../cabral/cabral.html">Cabral</a>
+                        <ul class="submenu">
+                            <li>
+                                3254-4224            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../juveve/">Juveve</a>
+                        <ul class="submenu">
+                            <li>
+                                3254-6621            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../patiom/">Patio Men</a>
+                        <ul class="submenu">
+                            <li>
+                                99774-0026            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../barigui/barigui.html">Barigui</a>
+                        <ul class="submenu">
+                            <li>
+                                99771-0437            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="../bariguim/bariguim.html">Barigui Men</a>
+                        <ul class="submenu">
+                            <li>
+                                3441-0211            
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </div>`
 
     // foooter-----------------------
-    footer.innerHTML = `<footer>       
-        <abbr title="T.i"><a href="informatica/pricipal.html"><img src="image/t.i.png" alt="Informatica"></a></abbr>
-        <abbr title="HOME"><a href=""><img src="image/home.png" alt="HOME"></a></abbr>
-        <abbr title="Galeria"><a href="galeria/principal.html"><img src="image/galeria.png" alt="Perguntas"></a></abbr>
-    </footer>`
+    footer.innerHTML = `
+        <abbr title="Estacionamento"><a href="../../estacionamento/etaunay.html"><img src="../../image/estacionamento.png" alt="Estacionamento"></a></abbr>
+        <abbr title="Dúvidas"><a href="../../duvidas.html"><img src="../../image/questions.png" alt="Dúvidas"></a></abbr>
+        <abbr title="HOME"><a href="../../index.html"><img src="../../image/home.png" alt="HOME"></a></abbr>
+        <abbr title="Lista tel"><a href="lista.tel.html"><img src="../../image/lista-tel.png" alt="Perguntas"></a></abbr>
+        <abbr title="Galeria"><a href="../galeria/principal.html"><img src="../../image/galeria.png" alt="Galeria"></a></abbr>`
 }
