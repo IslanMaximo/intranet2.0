30b118ea5f6d6fbf25d233985a74ec4c212a3baa
IslanMaximo <islanmvieira@gmail.com>
2025-08-06 16:43:52 -0300

    menu

--- script/header-footer/header-footer-principal.js
+++ script/header-footer/header-footer-principal.js
@@ -13,7 +13,7 @@ let header = document.querySelector('header')
         }else{
             menu.style.display = 'inline-block'
             header.style.width ='96px'
-            header.style.height ='310px'
+            header.style.height ='340px'
             header.style.background = 'rgba(128, 128, 128, 0.900)'
 
         }
